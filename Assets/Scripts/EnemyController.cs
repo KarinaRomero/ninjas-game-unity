389d319f4d2b70ac203d4e61b8448f14dae92ff6
fenix <betzy_skamm@hotmail.com>
2017-10-17 09:55:33 -0700

    initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rgb2d;
+	private Animator animator;
+
+	public Slider slider;
+	public Text energyText;
+
+	public float energy;
+
+	// Use this for initialization
+	void Start () {
+		speed = -1f;
+		energy = 100;
+		rgb2d = GetComponent <Rigidbody2D>();
+		animator = GetComponent <Animator> ();
+	}
+
+	void Update(){
+		slider.value = energy;
+		energyText.text = energy.ToString ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 vector = new Vector2 (speed,0);
+		rgb2d.velocity = vector;
+
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("walk") && Random.value < 1f/(60f*3f)) {
+			animator.SetTrigger ("jump");
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		Flip ();
+	}
+
+	void Flip(){
+		speed *= -1;
+
+		var s = transform.localScale;
+		s.x *= -1;
+
+		transform.localScale = s;
+	}
+}
+
---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerContoller : MonoBehaviour {
+
+	private Rigidbody2D rgb2d;
+	private Animator animator;
+	public float speedValue;
+	private bool moveRigth;
+
+	public Slider slider;
+	public Text energyText;
+
+	public float energy;
+
+	// Use this for initialization
+	void Start () {
+		moveRigth = true;
+		energy = 100;
+		rgb2d = GetComponent<Rigidbody2D> ();
+		animator = GetComponent <Animator> ();
+	}
+
+	void Update(){
+		slider.value = energy;
+		energyText.text = energy.ToString ();
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		float inputValue = Input.GetAxis ("Horizontal");
+
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Idle")) {
+			animator.SetTrigger ("Walk");
+		} else if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Walk")) {
+			animator.SetTrigger ("Idle");
+		}
+
+		Vector2 speed = new Vector2 (0, rgb2d.velocity.y);
+
+		inputValue *= speedValue;
+
+		speed.x = inputValue;
+
+		rgb2d.velocity = speed;
+
+		animator.SetFloat ("speed", speed.x);
+
+		if (moveRigth && inputValue < 0) {
+			moveRigth = false;
+			 
+			Flip ();
+		} else if (!moveRigth && inputValue > 0) {
+			moveRigth = true;
+			Flip ();
+		} 
+
+
+	}
+
+	void Flip(){
+		var s = transform.localScale;
+		s.x *= -1;
+		transform.localScale = s;
+
+
+	}
+}
